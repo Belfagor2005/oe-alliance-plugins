066b05e30910009fb0d3d956c9f2de49f4a5b978
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-05-02 15:19:02 +0200

    fix progressbar- skin - download - counter - set piconpath

--- PiconManager/src/__init__.py
+++ PiconManager/src/__init__.py
@@ -23,3 +23,33 @@ def _(txt):
 
 localeInit()
 language.addCallback(localeInit)
+
+
+DEFAULT_PICON_PATH = '/usr/share/enigma2/'
+ALTERN_PICON_PATH = [
+	'/usr/share/enigma2/',
+	'/media/usb/',
+	'/media/hdd/',
+	'/',
+	'/data/',
+	'/media/mmc/',
+	'/media/sdcard/',
+	'/media/hdd/XPicons/',
+	'/media/hdd/ZZPicons/',
+	'/media/usb/XPicons/',
+	'/media/usb/ZZPicons/',
+	'/usr/share/enigma2/XPicons/',
+	'/usr/share/enigma2/ZZPicons/',
+	'user_defined'
+]
+
+
+def getConfigPathList():
+	ChoicePath = []
+	for path in ALTERN_PICON_PATH:
+		if len(path) == 2:
+			ChoicePath.append(path)
+		else:
+			ChoicePath.append((path, _(path)))
+
+	return ChoicePath
